946ed7eb4e331716c7b3b7777c041450bf1260c9
thekid510 <76237067+thekid510@users.noreply.github.com>
2022-01-16 21:41:03 -0800

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,8 +18,8 @@ function find() {
       }
     ]
    */
-  return db('users').
-  join('roles','user_id', 'roles.role_id')
+  return db('users')
+  .join('roles','users.role_id', 'roles.role_id')
   .select('user_id','username','role_name')
 }
 
